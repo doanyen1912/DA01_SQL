f1df96ca0f75c02b39a4f12ca8ac38635364b908
doanyen1912 <117963268+doanyen1912@users.noreply.github.com>
2023-11-08 22:58:00 +0700

    Update ex1.sql

--- ex1.sql
+++ ex1.sql
@@ -1,3 +1,9 @@
 --EX1: 
 select name from city 
 where population > 120000 and countrycode = 'USA';
+--EX2:
+select * from city 
+where countrycode = 'JPN';
+--EX3:
+select city, state from station;
+--EX4:
