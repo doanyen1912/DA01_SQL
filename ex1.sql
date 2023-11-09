701c6a3b16f0cac0488a653975951bcfcef5a294
doanyen1912 <117963268+doanyen1912@users.noreply.github.com>
2023-11-09 07:41:27 +0700

    Update ex1.sql

--- ex1.sql
+++ ex1.sql
@@ -7,3 +7,16 @@ where countrycode = 'JPN';
 --EX3:
 select city, state from station;
 --EX4:
+select distinct city from station 
+where city like in ('%i','%e','%a','%e','%i','%u');
+--EX5:
+select distinct city 
+from station 
+where city like in ('%a','%e','%i','%o','%u');
+--EX6:
+Select DISTINCT CITY
+FROM
+STATION
+Where CITY NOT LIKE 'A%' AND CITY NOT LIKE 'E%' AND CITY NOT LIKE 'I%' AND CITY NOT LIKE 'O%' AND CITY NOT LIKE 'U%';\
+--EX7:
+
