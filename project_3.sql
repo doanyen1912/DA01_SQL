b73e84aaaef1829b644ba708e1e66f1e86bb6a7e
doanyen1912 <117963268+doanyen1912@users.noreply.github.com>
2023-12-06 10:38:53 +0700

    Update project_3.sql

--- project_3.sql
+++ project_3.sql
@@ -41,3 +41,27 @@ where state <> 'Tokyo' and state <> 'Osaka' and
 rank_sale = 1
 --câu này e tính ra sp có doanh thu cao nhất e thấy có tokyo vs osaka là k p uk nên e lọc 2 nc này ạ --  
 
+/* Ai là khách hàng tốt nhất, phân tích dựa vào RFM sl */ 
+--select * from public.segment_score
+
+with customer_rfm as (select 
+customername,
+current_date - max(orderdate ) as R,
+count (distinct ordernumber) as F,
+sum(sales) as M
+from public.sales_dataset_rfm_prj_clean_2
+group by customername ) 
+, rfm_score as (select customername,
+ntile(5) over(order by R desc) as R_score,
+ntile(5) over(order by F desc) as F_score,
+ntile(5) over(order by M desc) as M_score
+from customer_rfm ) 
+,rfm_final as (select customername,
+cast(R_score as varchar) || cast(F_score as varchar) ||cast(M_score as varchar) as rfm_final_score
+from rfm_score)
+
+select a.customername, b.segment from 
+rfm_final as a 
+join segment_score as b
+on a.rfm_final_score = b.scores
+where segment = 'Champions'
